cc98ec62b9e1f593653a0a51aed1090c924c73af
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-02-26 16:10:22 +0530

    partail template snippet

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
     ]
