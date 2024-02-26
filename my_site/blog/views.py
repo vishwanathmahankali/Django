1a7c0ee3f074b514a5b02cf12ae88b0cc944ed7b
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-02-26 20:38:24 +0530

    adding dummy data to views

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,4 +1,33 @@
+from datetime import date
 from django.shortcuts import render
+
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Vishwanath",
+        "date": date(2023, 7, 8),
+        "title": "Mountain Hiking",
+        "excerpt": """
+        There's nothing like the views you get when hiking in the mountains!
+          And I wasn't even prepared for what happened whilst I was enjoying the
+          view!
+        """,
+        "content": """
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+        aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+        velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+        aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+        velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+        aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+        velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
 
 # Create your views here.
 
