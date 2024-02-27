c7580e48c0da2dcf0d313e36624af046e131b299
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-02-27 20:56:30 +0530

    author model & one-to-many

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -3,6 +3,11 @@ from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
@@ -10,4 +15,4 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
