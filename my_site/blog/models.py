ff663d7ac20073aa000f7968576e66adccbd36dd
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-02-27 23:16:10 +0530

    fetching posts for starting page

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -6,10 +6,19 @@ from django.core.validators import MaxLengthValidator, MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.caption
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
@@ -18,5 +27,7 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts", null=True)
     tags = models.ManyToManyField(Tag)
+    
+    
