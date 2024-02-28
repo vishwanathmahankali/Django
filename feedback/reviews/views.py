6501ce5fae24a0135784433f0d83cd0c4ea463d4
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-02-28 13:59:28 +0530

    Form view

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,32 +1,40 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 from .models import Review
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
-
+from django.views.generic.edit import FormView
 from .forms import ReviewForm
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+class ReviewView(FormView):
     
-        return render(request, "reviews/review.html", {
-            "form": form,
-        })
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
     
-    def post(self, request):
-        form = ReviewForm(request.POST)
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    # def get(self, request):
+    #     form = ReviewForm()
+    
+    #     return render(request, "reviews/review.html", {
+    #         "form": form,
+    #     })
+    
+    # def post(self, request):
+    #     form = ReviewForm(request.POST)
         
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        return render(request, "reviews/review.html", {
-            "form": form,
-        })
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect("/thank-you")
+    #     return render(request, "reviews/review.html", {
+    #         "form": form,
+    #     })
     
     
 class ThankYouView(TemplateView):
