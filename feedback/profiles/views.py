136358080a4978f39f863bf6ab846d9c73f805bb
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-02-28 15:13:48 +0530

    using a create view

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 
 from .forms import ProfileForm
 from .models import UserProfile
+from  django.views.generic.edit import CreateView
 # Create your views here.
 
 # def store_file(file):
@@ -11,22 +12,30 @@ from .models import UserProfile
 #         for chunk in file.chunks():
 #             dest.write(chunk)
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form,
-        })
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form,
+#         })
+
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
         
-        if submitted_form.is_valid():
-            # store_file(request.FILES['image'])
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
+#         if submitted_form.is_valid():
+#             # store_file(request.FILES['image'])
+#             profile = UserProfile(image=request.FILES['user_image'])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
 
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
+        
+        
