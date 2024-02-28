a3c7c882b910abdb2f19b95c01e0ca1de2cf570d
vishwanathmahankali <vishwanathmahankali@gmail.com>
2024-02-28 13:33:37 +0530

    using the template view

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.ReviewView.as_view()),
-    path("thank-you", views.ThankYouView.as_view())
+    path("thank-you", views.ThankYouView.as_view()),
+    path("reviews", views.ReviewsListView.as_view()),
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -33,3 +33,13 @@ class ThankYouView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['message'] = "This works!"
         return context
+
+
+class ReviewsListView(TemplateView):
+    template_name = "reviews/review_list.html"
+    
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context['reviews'] = reviews
+        return context
